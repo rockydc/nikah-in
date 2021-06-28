0f8a907df21c661256867a2e61172fb31e067bdf
Rocky Daniel <rockydaniel182@gmail.com>
2021-06-28 22:51:54 +0700

    tambahin modal prokes
    modal prokes

---
+++ resources/views/layouts/user/anesWeb.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Yohanes &amp; Nahomi</title>
+    <meta name="description" content="Yohanes &amp; Nahomi Wedding">
+    <link rel="icon" type="image/png" sizes="500x500" href="{{url('aness/assets/img/icon.png')}}">
+    <link rel="stylesheet" href="{{url('aness/assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aclonica">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Andika">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Beth+Ellen">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Damion">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Herr+Von+Muellerhoff">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Homemade+Apple">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Josefin+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Meddon">
+    <link rel="stylesheet" href="{{url('aness/assets/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.css">
+    <link rel="stylesheet" href="{{url('aness/assets/css/styles.css')}}">
+    <link rel="stylesheet" href="{{url('aness/assets/css/styles.compiled.css')}}">
+    <link rel="stylesheet" href="{{url('aness/assets/scss/styles.css')}}">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7YLNT1MN0T"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7YLNT1MN0T');
+    </script>
+
+
+
+</head>
+
+<audio loop id="myaudio" type="audio/mp3">
+    <source src="{{url('aness/music/Isyana Sarasvati, Afgan, Rendy Pandugo - Heaven (Official Music Video).mp3')}}">
+</audio>
+
+<body id="page-top" style="background: rgb(246,236,226);">
+  
+    @include('includes.anes.navbar')
+    @yield('content')
+    <footer class="text-center">
+ 
+
+
+        <div>
+            <p style="font-family: 'Josefin Sans', sans-serif;"><em>© &nbsp;nikah-in.com 2020. All rights reserved.</em><br></p>
+            <!-- <iframe src="{{url('aness/music/Isyana Sarasvati, Afgan, Rendy Pandugo - Heaven (Official Music Video).mp3')}}" allow="autoplay" style="display:none" id="iframeAudio">
+            </iframe>  -->
+            <button class="act-btn text-center"type="button" id="playbutton">
+            <i class="fa fa-play" id="icon-play"></i>
+            </button>
+        </div>
+    </footer>
+
+    <script src="{{url('aness/assets/js/jquery.min.js')}}"></script>
+    <script src="{{url('aness/assets/js/bs-init.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.2.0/aos.js"></script>
+    <script src="{{url('aness/assets/js/countdown.js')}}"></script>
+    <script src="{{url('aness/assets/js/scrolling.js')}}"></script>
+    <script src="{{url('aness/assets/js/scrollchangecolor.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script>
+   
+
+        var myaudio = document.getElementById("myaudio");
+        var iconplay = document.getElementById("icon-play");
+
+        
+
+
+        playbutton.addEventListener("click",()=>{
+            if(myaudio.paused){
+                myaudio.play();
+                $('#icon-play').removeClass('fa-play');
+                $('#icon-play').addClass('fa-pause')
+            }else{
+                myaudio.pause();
+                $('#icon-play').removeClass('fa-pause');
+                $('#icon-play').addClass('fa-play');
+
+            }
+        })
+       
+       </script>
+
+
+    
+    <script src="{{url('aness/assets/bootstrap/js/bootstrap.min.js')}}"></script>
+</body>
+
+</html>
